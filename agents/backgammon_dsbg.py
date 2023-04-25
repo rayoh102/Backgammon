81d6a345483c7a0bb55f8a148ceacc296a788822
rayoh102 <rayoh7@gmail.com>
2023-04-24 19:30:17 -0700

    finished dsbg ssbg

--- agents/backgammon_dsbg.py
+++ agents/backgammon_dsbg.py
@@ -4,16 +4,19 @@ UW netid(s): rayoh101
 '''
 
 from game_engine import genmoves
+import math
 
 class BackgammonPlayer:
     def __init__(self):
         self.GenMoveInstance = genmoves.GenMoves()
         # feel free to create more instance variables as needed.
         self.AlphaBeta = False
+        self.staticEvalFunction = None
         self.states = 0
         self.cutoffs = 0
         self.maxPly = 1
-        self.staticEvalFunction = None
+        self.die1 = 1
+        self.die2 = 6
 
     # TODO: return a string containing your UW NETID(s)
     # For students in partnership: UWNETID + " " + UWNETID
@@ -54,10 +57,160 @@ class BackgammonPlayer:
     def move(self, state, die1=1, die2=6):
         # TODO: return a move for the current state and for the current player.
         # Hint: you can get the current player with state.whose_move
-        pass
+        if self.AlphaBeta:
+            return self.alphaBetaPrune(state, 2, -10000000, 10000000, die1, die2)[1]
+        else:
+            return self.minimax(state, 2, die1, die2)[1]
 
-
+    # The function takes a board state state as input and returns an evaluation score for the state
     # Hint: Look at game_engine/boardState.py for a board state properties you can use.
     def staticEval(self, state):
         # TODO: return a number for the given state
-        return -1
+
+        listW =[]
+        listR=[]
+
+        for i in range(len(state.pointLists)):
+            countW = 0
+            countR = 0
+            homeCountW=0
+            homeCountR=0
+
+            for j in range(len(state.pointLists[i])):
+                if state.pointLists[i][j]==0:
+                    countW += 1
+                    if i<=5 and i>=0:
+                        homeCountW += 1
+                elif state.pointLists[i][j]==1:
+                    countR += 1
+                    if i>=18 and i<=23:
+                        homeCountR+=1
+
+            listW.append(countW)
+            listR.append(countR)
+
+        pipCountWhite = 0
+        pipCountRed = 0
+
+        for i in range(len(listW)):
+            pipCountWhite += listW[i] * (len(listW) -i)
+
+        for i in range(len(listR)):
+            pipCountRed += listR[i] * (i+1)
+
+        # Caclulate Evaluation Score
+        evalScore = (pipCountRed + 25*state.bar.count(1) + homeCountW + 25*len(state.white_off))
+        - (pipCountWhite + 25*state.bar.count(0) + homeCountR + 25*len(state.red_off))
+
+        return evalScore
+
+    def minimax(self, state, plyLeft, die1, die2):
+        if plyLeft == 0:
+            self.states += 1
+            if self.staticEvalFunction != None:
+                return (self.staticEvalFunction(state),"")
+            else:
+                return (self.staticEval(state),"")
+
+        # "bestMove" stores current best move and its value found
+        bestMove = (0,"")
+
+        currPlayer = state.whose_move
+
+        #Max player's (white) turn
+        if currPlayer == 0:
+            bestMove = (-math.inf,"")
+        #Min player's (red) turn
+        else:
+            bestMove = (math.inf,"")
+
+        # Iterate through all possible moves and call "minimax" recursively
+        for next_move, next_state in self.get_all_moves_state_pair(state, die1, die2):
+            # Resulting value of recursive call is stored in newMove
+            newMove = self.minimax(next_state, plyLeft-1, die1, die2)
+
+            # If Max player's turn and newMove's value is higher than current bestMove's value, update bestmove
+            if (currPlayer == 0) and newMove[0] > bestMove[0]:
+                bestMove = (newMove[0], next_move)
+
+            # If Min player's turn & newMove's value is less than current bestMove's value, update bestmove
+            elif (currPlayer ==1) and newMove[0] < bestMove[0]:
+                bestMove = (newMove[0], next_move)
+
+        return bestMove
+
+    def alphaBetaPrune(self, state, plyLeft, alpha, beta, die1, die2):
+        if plyLeft == 0:
+            self.states += 1
+            if self.staticEvalFunction != None:
+                return (self.staticEvalFunction(state),"")
+            else:
+                return (self.staticEval(state),"")
+
+        # "bestMove" stores current best move and its value found
+        bestMove = (0,"")
+
+        currPlayer = state.whose_move
+
+        #Max player's (white) turn
+        if currPlayer == 0:
+            bestMove = (-math.inf,"")
+
+            for next_move, next_state in self.get_all_moves_state_pair(state, die1, die2):
+                # Calls the alphabetaprune recursively with the next state and updates the alpha and beta values
+                newVal = self.alphaBetaPrune(next_state, plyLeft-1, alpha, beta, die1, die2)
+
+                # Update bestmove
+                if newVal[0] > bestMove[0]:
+                    bestMove = (newVal[0], next_move)
+
+                # Update alpha value and check for beta cutoff
+                alpha = max(alpha, bestMove[0])
+
+                if beta <= alpha:
+                    self.cutoffs += 1
+                    break
+
+            return bestMove
+
+        #Min player's (red) turn
+        else:
+            bestMove = (math.inf,"")
+
+            for next_move, next_state in self.get_all_moves_state_pair(state, die1, die2):
+                newVal = self.alphaBetaPrune(next_state, plyLeft-1, alpha, beta, die1, die2)
+
+                if newVal[0] < bestMove[0]:
+                    bestMove = (newVal[0], next_move)
+
+                beta = min(beta, bestMove[0])
+                if beta <= alpha:
+                    self.cutoffs += 1
+                    break
+
+            return bestMove
+
+    #This function takes in a current state of the board (curr_state) and the values of two dice
+    # (die1 and die2) and returns a list of all possible moves and their resulting states
+    def get_all_moves_state_pair(self, curr_state, die1, die2):
+        moveGen = self.GenMoveInstance.gen_moves(curr_state, curr_state.whose_move, die1, die2)
+        listOfMoves = []
+        finished = False
+        legalMoves = False
+
+        # Generate legal moves
+        while not finished:
+            try:
+                move = next(moveGen)
+                if move[0] != 'p':
+                    legalMoves = True
+                    listOfMoves .append(move)
+
+            # Exception raised when no more moves left
+            except StopIteration as e:
+                finished = True
+
+        # If no legal moves found, then pass
+        if not legalMoves:
+            listOfMoves.append(('p', curr_state))
+        return listOfMoves
--- agents/backgammon_ssbg.py
+++ agents/backgammon_ssbg.py
@@ -4,6 +4,7 @@ UW netid(s): rayoh101
 '''
 
 from game_engine import genmoves
+import math
 
 class BackgammonPlayer:
     def __init__(self):
@@ -37,10 +38,108 @@ class BackgammonPlayer:
     def move(self, state, die1, die2):
         # TODO: return a move for the current state and for the current player.
         # Hint: you can get the current player with state.whose_move
-        return 'q'
-
+        return self.expectiminimax(state, self.maxPly, die1, die2)[1]
 
     # Hint: Look at game_engine/boardState.py for a board state properties you can use.
     def staticEval(self, state):
         # TODO: return a number for the given state
-        return -1
+
+        listW=[]
+        listR=[]
+
+        for i in range(len(state.pointLists)):
+            countW = 0
+            countR = 0
+            homeCountW=0
+            homeCountR=0
+
+            for j in range(len(state.pointLists[i])):
+                if state.pointLists[i][j]==0:
+                    countW += 1
+                    if i<=5 and i>=0:
+                        homeCountW += 1
+                elif state.pointLists[i][j]==1:
+                    countR += 1
+                    if i>=18 and i<=23:
+                        homeCountR+=1
+
+            listW.append(countW)
+            listR.append(countR)
+
+        pipCountWhite = 0
+        pipCountRed = 0
+
+        for i in range(len(listW)):
+            pipCountWhite += listW[i] * (len(listW) -i)
+
+        for i in range(len(listR)):
+            pipCountRed += listR[i] * (i+1)
+
+        # Caclulate Evaluation Score
+        evalScore = (pipCountRed + 25*state.bar.count(1) + homeCountW + 25*len(state.white_off))
+        - (pipCountWhite + 25*state.bar.count(0) + homeCountR + 25*len(state.red_off))
+
+        return evalScore
+
+    def get_all_moves_state_pair(self, curr_state, die1, die2):
+        """Uses the mover to generate all legal (moves,state) pairs."""
+        #generate move for curr_state
+        moveGen = self.GenMoveInstance.gen_moves(curr_state, curr_state.whose_move, die1, die2)
+        listOfMoves = []
+        finished = False
+        legalMoves = False
+
+        # Generate legal moves
+        while not finished:
+            try:
+                move = next(moveGen)
+                if move[0] != 'p':
+                    legalMoves = True
+                    listOfMoves .append(move)
+
+            # Exception raised when no more moves left
+            except StopIteration as e:
+                finished = True
+
+        # If no legal moves found, then pass
+        if not legalMoves:
+            listOfMoves.append(('p', curr_state))
+        return listOfMoves
+
+    def expectiminimax(self, state, plyLeft, die1, die2):
+        currPlayer = state.whose_move
+
+        # If plyLeft = 0, returns the static evaluation of the current state
+        if plyLeft == 0:
+            if self.staticEvalFunction != None:
+                return (self.staticEvalFunction(state),"")
+            else:
+                return (self.staticEval(state),"")
+
+        # bestMove is the current best move and its value
+        bestMove = (0,"")
+
+        #Max player's (white) turn
+        if currPlayer == 0:
+            bestMove = (-math.inf,"")
+        #Min player's (red) turn
+        else:
+            bestMove = (math.inf,"")
+
+        for next_move, next_state in self.get_all_moves_state_pair(state, die1, die2):
+            newVal = 0
+
+            for d1 in range(6):
+                for d2 in range(6):
+                    temp = self.expectiminimax(next_state, plyLeft-1, d1, d2)
+                    newVal += temp[0] / 36
+
+            # Update best move if a better move is found
+            if (currPlayer == 0) and newVal > bestMove[0]:
+                bestMove = (newVal, next_move)
+
+            elif (currPlayer ==1) and newVal < bestMove[0]:
+                bestMove = (newVal, next_move)
+
+        return bestMove
+
